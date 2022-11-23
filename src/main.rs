7d230f8693f2a987fe258e6b491b3e6702d98f59
ChenYing Kuo <evshary@gmail.com>
2022-11-23 13:36:38 +0800

    Fix indent.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 
 fn print_help() {
-  println!("------------------------------------");
-  println!("| Different Mode:                  |");
-  println!("|   z: Toggle auto & external mode |");
-  println!("|   x: GateMode => Drive           |");
-  println!("|   c: GateMode => Reverse         |");
-  println!("|   v: GateMode => Park            |");
-  println!("|   s: View current mode           |");
-  println!("| Speed:                           |");
-  println!("|   u: Increase speed              |");
-  println!("|   i: Set speed to 0              |");
-  println!("|   o: Decrease speed              |");
-  println!("| Steering Angle                   |");
-  println!("|   j: Left turn                   |");
-  println!("|   k: Set angle to 0              |");
-  println!("|   l: Right turn                  |");
-  println!("------------------------------------");
+    println!("------------------------------------");
+    println!("| Different Mode:                  |");
+    println!("|   z: Toggle auto & external mode |");
+    println!("|   x: GateMode => Drive           |");
+    println!("|   c: GateMode => Reverse         |");
+    println!("|   v: GateMode => Park            |");
+    println!("|   s: View current mode           |");
+    println!("| Speed:                           |");
+    println!("|   u: Increase speed              |");
+    println!("|   i: Set speed to 0              |");
+    println!("|   o: Decrease speed              |");
+    println!("| Steering Angle                   |");
+    println!("|   j: Left turn                   |");
+    println!("|   k: Set angle to 0              |");
+    println!("|   l: Right turn                  |");
+    println!("------------------------------------");
 }
 
 fn main() {
