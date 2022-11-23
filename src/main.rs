7fcf73b36720c104786c6538fbeb279162d39e68
ChenYing Kuo <evshary@gmail.com>
2022-11-23 11:25:54 +0800

    Able to detect keyboard input.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+
 fn print_help() {
   println!("------------------------------------");
   println!("| Different Mode:                  |");
@@ -19,4 +21,48 @@ fn print_help() {
 
 fn main() {
     print_help();
+    crossterm::terminal::enable_raw_mode().unwrap();
+    loop {
+        match crossterm::event::read() {
+            Ok(Event::Key(KeyEvent { code: KeyCode::Char('c'), modifiers: KeyModifiers::CONTROL, kind: _, state: _})) => {
+                break;
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('z'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('x'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('c'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('v'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('s'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('u'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('i'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('o'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('j'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('k'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(Event::Key(KeyEvent {code: KeyCode::Char('l'), modifiers: _, kind: _, state: _})) => {
+                // Do something
+            },
+            Ok(_) => {},
+            Err(_) => {}
+        }
+    }
+    crossterm::terminal::disable_raw_mode().unwrap();
 }
